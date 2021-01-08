c509f0b54a31e046af65cc11c7a8a08f734d8f4b
general <general@Ren900Win10.com>
2021-01-08 21:35:49 +0800

    8 2021-01-08 21:39

--- src/main.cpp
+++ src/main.cpp
@@ -178,7 +178,24 @@ void MOVE1(int A[], int n, int k)
 }
 
 
+void REVERSE(int A[], int from, int to)
+{
+	int temp;
+	int i;
+	for(i = 0; i < (to-from+1)/2; i++)
+	{
+		temp = A[from+i];
+		A[from+i] = A[to-i];
+		A[to-i] = temp;
+	}
+}
 
+void MOVE2(int A[], int n, int k)
+{
+	REVERSE(A, n-k, n-1);
+	REVERSE(A, 0, n-k-1);
+	REVERSE(A, 0, n-1);
+}
 
 
 
